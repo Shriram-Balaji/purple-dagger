13fa543c328905fdc8c86758e68c95c0da64292d
Shriram Balaji <>
2018-01-01 21:12:24 +0530

    Updated Header style to use white background and smaller font size

--- app/src/components/Header/Header.jsx
+++ app/src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ class Header extends Component {
     return(
       <header className="navbar rad-navbar">
         <section className="navbar-section">
-          <a href="#" id="rad_icon" className="rad-header is-size-2 is-size-3-mobile">{this.props.logo}</a>
+          <a href="#" id="rad_icon" className="rad-header is-size-3 is-size-4-mobile">{this.props.logo}</a>
           <span id="nightMode"></span>
         </section>
       </header>             
